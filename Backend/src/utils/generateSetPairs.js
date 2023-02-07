f66656d8af8ce4b97df3ba2aac3adcd75b9c31a9
Lara Mauricio <laradcm@gmail.com>
2023-02-07 01:12:54 -0400

    added bikes post and delete, WIP for put

---
+++ Backend/src/utils/generateSetPairs.js
@@ -0,0 +1,30 @@
+
+
+// WIP
+// function generateSetPairs( inObj ){
+
+//     const entries = Object.entries( inObj );
+//     console.log(entries);
+
+
+
+
+//     const setArr = [];
+
+
+    
+
+//     console.log({
+//         keys,
+//         size,
+//         values
+//     });
+//     return {
+//         keys,
+//         size,
+//         values
+//     }
+
+// }
+
+// module.exports = generateSetPairs;
