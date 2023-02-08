5119e487846bb547d52c9288ebf003628b31237e
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-07 21:40:23 -0500

    update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// fetch("http://127.0.0.1:3000/bicycles ")
-// .then((res) => {
-
-//   console.log('got here');
-//   return res.json();
-
-// })
-// .then((result) => {
-//   console.log(result);
-// }); 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
