a374ec4199f16c7ea4079cffe4d45b8a0d9a1f10
charles-édouard <59705750+ccbbccbb@users.noreply.github.com>
2023-02-07 21:06:08 -0500

    Update db.js

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -1,10 +1,9 @@
-
 const db = {
-    host: '127.0.0.1',
-    database: 'VeloCity',
-    port: 5432,
-    user: 'postgres',
-    password:'admin'
+  host: "127.0.0.1",
+  database: "velocity",
+  port: 5432,
+  user: "postgres",
+  password: "admin",
 };
 
 module.exports = db;
