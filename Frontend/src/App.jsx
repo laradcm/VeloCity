6f86dbd6bf313c4a5a1e80fe2e40eebcd3b04ce6
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-07 21:39:13 -0500

    changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const url = "http://127.0.0.1:3000/bicycles";
+// const url = 'https://course-api.com/react-tours-project'
 
+function App() {
+  const [loadData, setLoadData] = useState(true);
+
+  const fetchBikes = async () => {
+    setLoadData(true);
+    try {
+    const response = await fetch(url);
+    const bikes = await response.json();
+    // to see the data fetched in the console
+    console.log(bikes);
+
+    } catch (error) {
+      setLoadData(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchBikes();
+  }, []);
+
+  // if there's an error while fetching the data
+  if (!loadData){
+    return (
+      <>
+        <h1>Error occurred while fetching data</h1>
+      </>
+    )
+  }
+
+  // data was fetched correctly
   return (
-    <div className="App">
-    </div>
+    <>
+      <h1>data -bikes- fetched correctly</h1>
+    </>
   )
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-fetch("http://127.0.0.1:3000/bicycles ")
-.then((res) => {
+// fetch("http://127.0.0.1:3000/bicycles ")
+// .then((res) => {
 
-  console.log('got here');
-  return res.json();
+//   console.log('got here');
+//   return res.json();
 
-})
-.then((result) => {
-  console.log(result);
-}); 
+// })
+// .then((result) => {
+//   console.log(result);
+// }); 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
