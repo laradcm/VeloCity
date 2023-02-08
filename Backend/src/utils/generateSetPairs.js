ff88251a8545029bf2c8a1ba8c4c62eb6153d90e
lafontaju <lafontaju@gmail.com>
2023-02-08 02:08:18 -0500

    bicycles UPDATE operation

--- Backend/src/utils/generateSetPairs.js
+++ Backend/src/utils/generateSetPairs.js
@@ -1,30 +1,33 @@
 
+function generateSetPairs( inObj ){
 
-// WIP
-// function generateSetPairs( inObj ){
+    const entries = Object.entries( inObj );
 
-//     const entries = Object.entries( inObj );
-//     console.log(entries);
+    const keys = Object.keys( inObj );
+    const size = keys.length;
+    const values =  Object.values( inObj );
 
+    const setPairs = [];
+    let wherePair;
 
+    let ref = 2;
 
+    for ( const key of keys ) {
+      if (key !== "id") {
+        setPairs.push(key + ` = $${ref}`);
+        ref++;
+      } else {
+        wherePair = key + ` = $1`;
+      }
+    }
 
-//     const setArr = [];
+    return {
+        setPairs,
+        wherePair,
+        size,
+        values
+    }
 
+}
 
-    
-
-//     console.log({
-//         keys,
-//         size,
-//         values
-//     });
-//     return {
-//         keys,
-//         size,
-//         values
-//     }
-
-// }
-
-// module.exports = generateSetPairs;
+module.exports = generateSetPairs;
