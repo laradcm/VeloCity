1a0388ac2e486889e81dde690d508b5cb20e1b05
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-08 20:37:56 -0500

    profile page test

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,3 +1,51 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Fisrt Name", "Harry"),
+  createData("Last Name", "Potter"),
+  createData("Email", "kittycat@example.ca"),
+  createData("Phone Number", "123-456-7890"),
+  createData("Billing Address", "P. Sherman, 42 Wallaby Way, Sydney"),
+  createData("Password", "********"),
+];
+
 export function Profile() {
-  return <h1>User profile pageee</h1>;
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="caption table">
+        <caption>Edit</caption>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <strong style={{ textTransform: "uppercase" }}>
+                Personal information
+              </strong>
+            </TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
