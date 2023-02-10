d0be57a594b2594d3dd7c802eae720ab32c5282b
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-09 22:51:38 -0500

    update 5.0

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,38 +1,59 @@
 import { Button, Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        Vélocity
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 export function Home() {
-    return (
-        <>
-            <br></br>
-            <Card>
-                <Card.Body>
-                    <Container>
-                        <h1>Welcome to Vélocity</h1>
-                        <Card.Body></Card.Body>
-                        <Row>
-                            <Col>
-                                <h3>Already a member?</h3>
-                                <p>Book a ride now.</p>
-                                <Link to="/signin">
-                                    <Button>Sign In</Button>
-                                </Link>
-                            </Col>
-                            <Col>
-                                <h3>Don't have an account?</h3>
-                                <p>
-                                    Start profiting from a network of thousands
-                                    of bikes around the city.
-                                </p>
-                                <Link to="/signup">
-                                    <Button>Sign Up</Button>
-                                </Link>
-                            </Col>
-                        </Row>
-                        <Card.Body></Card.Body>
-                    </Container>
-                </Card.Body>
-            </Card>
-        </>
-    );
+  return (
+    <>
+      <br></br>
+      <Paper className="ccontainer cccontainer">
+        {/* <Card>
+          <Card.Body> */}
+        <Container>
+          <h1>Welcome to Vélocity</h1>
+          <Card.Body></Card.Body>
+          <Row>
+            <Col>
+              <h3>Already a member?</h3>
+              <p>Book a ride now.</p>
+              <Link to="/signin">
+                <Button>Sign In</Button>
+              </Link>
+            </Col>
+            <Col>
+              <h3>Don't have an account?</h3>
+              <p>
+                Start profiting from a network of thousands of bikes around the
+                city.
+              </p>
+              <Link to="/signup">
+                <Button>Sign Up</Button>
+              </Link>
+            </Col>
+          </Row>
+          <Card.Body></Card.Body>
+        </Container>
+        {/* </Card.Body>
+        </Card> */}
+      </Paper>
+      <Box paddingTop={"1rem"}>
+        <Copyright />
+      </Box>
+    </>
+  );
 }
--- Frontend/src/pages/ReportBike.jsx
+++ Frontend/src/pages/ReportBike.jsx
@@ -1,3 +1,44 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import { fontSize } from "@mui/system";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { Card } from "react-bootstrap";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="/main">
+        Vélocity
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
 export function ReportBike() {
-  return <h1>Got any problem? Report your bike here!!!!</h1>;
+  return (
+    <>
+      <Paper className="ccontainer">
+        <Typography component="h1" variant="h4" align="center">
+          Contact support
+        </Typography>
+        <Container>
+          Something not working well? <br></br>
+          Do you want to share feedback to improve our service?<br></br>
+          Send us a message and we’ll get in touch with you very soon!
+        </Container>
+      </Paper>
+      <Copyright />
+    </>
+  );
 }
