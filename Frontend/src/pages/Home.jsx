c511142424189449811ae419958f189224964dd7
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-13 15:23:32 -0500

    fixed buttons not redirecting

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,37 +3,37 @@ import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 
 export function Home() {
-    return (
-        <>
-            <Paper className="MainContentContainer">
-                <Container>
-                    <Row>
-                        <Col>
-                            <h3>Already a member?</h3>
-                            <p>Book a ride now.</p>
-                        </Col>
-                        <Col>
-                            <h3>Don't have an account?</h3>
-                            <p>
-                                Start profiting from a network of thousands of
-                                bikes around the city.
-                            </p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Link to="/signin">
-                                <Button>Sign In</Button>
-                            </Link>
-                        </Col>
-                        <Col>
-                            <Link to="/signup">
-                                <Button>Sign Up</Button>
-                            </Link>
-                        </Col>
-                    </Row>
-                </Container>
-            </Paper>
-        </>
-    );
+  return (
+    <>
+      <Paper className="MainContentContainer">
+        <Container>
+          <Row>
+            <Col>
+              <h3>Already a member?</h3>
+              <p>Book a ride now.</p>
+            </Col>
+            <Col>
+              <h3>Don't have an account?</h3>
+              <p>
+                Start profiting from a network of thousands of bikes around the
+                city.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Link href="/signin">
+                <Button>Sign In</Button>
+              </Link>
+            </Col>
+            <Col>
+              <Link href="/signup">
+                <Button>Sign Up</Button>
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </Paper>
+    </>
+  );
 }
