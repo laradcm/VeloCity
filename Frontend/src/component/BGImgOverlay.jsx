af4a59b4eaf7f30147dadcb369b4172d001255c2
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-13 15:06:31 -0500

    changed path of pictures because i was receiving error

--- Frontend/src/component/BGImgOverlay.jsx
+++ Frontend/src/component/BGImgOverlay.jsx
@@ -2,9 +2,9 @@ import { Container, Card } from "react-bootstrap";
 // Background image credit goes to the artist Jon&Roy, its a slight variation of some of their art.
 
 export function BGImgOverlay() {
-    return (
-        <Container id="BGContainer" className="fixed-bottom">
-            <img src="public\Background.png" id="BGImage"></img>
-        </Container>
-    );
+  return (
+    <Container id="BGContainer" className="fixed-bottom">
+      <img src="/Background.png" id="BGImage"></img>
+    </Container>
+  );
 }
--- Frontend/src/component/FootBar.jsx
+++ Frontend/src/component/FootBar.jsx
@@ -3,29 +3,29 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 
 function Copyright() {
-    return (
-        <Typography color="#ffffff" align="center" id="Foot">
-            <Link color="#ffffff" href="/">
-                Vélocity
-            </Link>
-            &nbsp;{"©"}&nbsp;
-            {new Date().getFullYear()}&nbsp;
-        </Typography>
-    );
+  return (
+    <Typography color="#ffffff" align="center" id="Foot">
+      <Link color="#ffffff" href="/">
+        Vélocity
+      </Link>
+      &nbsp;{"©"}&nbsp;
+      {new Date().getFullYear()}&nbsp;
+    </Typography>
+  );
 }
 
 export function FootBar() {
-    return (
-        <Navbar className="fixed-bottom navbar navbar-dark color-nav">
-            <Container>
-                <Nav>
-                    <Copyright />
-                </Nav>
-                <Nav></Nav>
-                <Nav>
-                    <img src="\public\wheel-logo-white.png" height="50px"></img>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar className="fixed-bottom navbar navbar-dark color-nav">
+      <Container>
+        <Nav>
+          <Copyright />
+        </Nav>
+        <Nav></Nav>
+        <Nav>
+          <img src="/wheel-logo-white.png" height="50px"></img>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
