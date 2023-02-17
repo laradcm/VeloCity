98b78e1eaeab42531f03a97a904331ad9e368adc
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-02-16 20:17:22 -0500

    fixed nav bar and sign out

--- Frontend/src/pages/SignOut.jsx
+++ Frontend/src/pages/SignOut.jsx
@@ -2,32 +2,45 @@ import { Button, Container, Row, Col, Card } from "react-bootstrap";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import { useCookies } from "react-cookie"; // cookies
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
+import { useNavigate } from "react-router-dom"; // to redirect
+import Box from "@mui/material/Box";
 
 function AlertMessageSuccess() {
   return (
     <>
-      <Stack sx={{ width: "100%" }} marginTop="1rem">
-        <Alert severity="success">
-          There were errors while creating your account, please verify your data
-          and try again.
-        </Alert>
+      <Stack sx={{ width: "100%" }} marginTop="1rem" alignItems={"center"}>
+        <Alert severity="success">You are succesfully logged out.</Alert>
       </Stack>
+      <Box marginTop="2rem">
+        <Row>
+          <Col>
+            <h6>Do you want to sign in again?</h6>
+          </Col>
+        </Row>
+        <Row style={{ paddingTop: "0.5rem" }}>
+          <Col>
+            <Link href="/signin">
+              <Button>Sign in</Button>
+            </Link>
+          </Col>
+        </Row>
+      </Box>
     </>
   );
 }
 
 export function SignOut() {
-  const [displaySuccessAlert, setDisplaySuccessAlert] = useState(false);
+  const [displaySuccessAlert, setDisplaySuccessAlert] = useState(false); // display alert
   const [cookies, setCookie, removeCookie] = useCookies(["user"]); // cookies
 
   function deleteCookie() {
     removeCookie("email");
     setDisplaySuccessAlert(true);
-    console.log("this was accioned");
   }
+
   return (
     <>
       <Paper className="MainContentContainer centereddd" sx={{ maxWidth: 600 }}>
@@ -45,11 +58,6 @@ export function SignOut() {
             </Col>
           </Row>
           {displaySuccessAlert ? <AlertMessageSuccess /> : null}
-          <Row style={{ paddingTop: "2rem" }}>
-            <Link href="/main">
-              <Button>No, take me back</Button>
-            </Link>
-          </Row>
         </Container>
       </Paper>
     </>
